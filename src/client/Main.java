b9539eeb2fa323bda96386295c9079f2e847f358
Joydeep Gill <joyydeep.gill@gmail.com>
2025-08-23 19:33:42 -0400

    Made the base skeleton class for parsing the command line

---
+++ src/client/Main.java
@@ -0,0 +1,15 @@
+package client;
+
+public class Main {
+
+    public static void main(String[] args) {
+    /*
+     * Parses the command line arguments, 
+     * and call the encryption function to encrypt file contents. 
+     */ 
+    
+     
+
+
+    }   
+} 
