cd668eecd626f96db3d23ed3c6fa47b536d5d176
Joydeep Gill <joyydeep.gill@gmail.com>
2025-08-24 18:00:27 -0400

    Modified file and directory structure and installed maven.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,9 @@
 package client;
 
+// Download Apache maven and add org.apache in our classpath 
+import org.apache.commons.cli.*; 
+
+
 public class Main {
 
     public static void main(String[] args) {
